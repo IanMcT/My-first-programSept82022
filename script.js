85aa9aa816b9331d605ec28df56973cedca68321
IanMcT <ian.mctavish@tldsb.on.ca>
2022-09-08 15:24:58 +0000

    Finished the program

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-alert("Hi, Ian McTavish");
-document.getElementById("output").innerHTML = "Ian McTavish";
+
+function sayHi(){
+  //alert("in sayHi method");
+  var name;
+  name = document.getElementById("inputName").value;
+  alert(name);
+  var age;
+  age = parseInt(document.getElementById("inputAge").value);
+  alert(age+1);
+  document.getElementById("output").innerHTML = "Hello, " + name +
+    ". You are " + age + " years old.  Next year you will be " + (age+1);
+}
+
+
